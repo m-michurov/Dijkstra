dd4f38e1dd6f0f1922855a2877f0bf025573cda5
m-michurov <>
2019-03-31 23:19:20 +0700

    ugh
    what now?

--- main.c
+++ main.c
@@ -45,8 +45,8 @@ int main(
         src--;
         dst--;
 
-        graph->adjacency_matrix[src * graph->vertices + dst] = (unsigned int) length;
-        graph->adjacency_matrix[dst * graph->vertices + src] = (unsigned int) length;
+        graph->adjacency_matrix[src * (int) graph->vertices + dst] = (unsigned int) length;
+        graph->adjacency_matrix[dst * (int) graph->vertices + src] = (unsigned int) length;
     }
 
     result = FindDistance(graph, (short) end);
